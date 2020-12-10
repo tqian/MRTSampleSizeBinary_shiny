b7448a63636a42d62f71999cf4ec8078b407ed71
ewongtoi <ewongtoi@gmail.com>
2020-12-09 21:05:34 -0800

    replaced functions that were rewritten in the mrtbincalc package

--- sample_size_calc_bin_mrt.R
+++ sample_size_calc_bin_mrt.R
@@ -1,3 +1,4 @@
+library(mrtbincalc)
 
 if (0) {
     total_T <- 30
@@ -54,51 +55,18 @@ calculateSampleSizeBinaryMRT_wrapper <- function(p10, pT0, p11, pT1,
         f_t <- cbind(1, 1:total_T)
     }
     
-    sample_size <- MRT_bin_samplesize.F(avail_pattern = avail_pattern,
+    sample_size <- calculate_mrt_bin_samplesize_f(avail_pattern = avail_pattern,
                                         f_t = f_t,
                                         g_t = g_t,
                                         beta = alpha_beta_ate$beta,
                                         alpha = alpha_beta_ate$alpha,
                                         p_t = rand_prob,
                                         gamma = typeIerror,
-                                        b = 1 - power)
-    return(ceiling(sample_size))
-}
-
-
-# Calculate sample size with given power
-MRT_bin_samplesize.F <- function(avail_pattern,  ## E[I_t]  # TQ: will assume this is vector of length T
-                                 f_t,             ## f(t)    # TQ: will assume this f_t object is matrix of dimension T * p
-                                 g_t,             ## g(t)    # TQ: will assume this g_t object is matrix of dimension T * q
-                                 beta,            ## beta_0
-                                 alpha,           ## alpha_0
-                                 p_t,             ## p_t (randomization probability)
-                                 gamma,          ## gamma (type I error rate),
-                                 b)               ## b (Type II error rate)
-{
-    
-    p <- length(beta)
-    q <- length(alpha)
-    
-    M_and_Sigma <- computeMSigma(avail_pattern, f_t, g_t, beta, alpha, p_t)
-    M.Matrix <- M_and_Sigma$M
-    Sigma.Matrix <- M_and_Sigma$Sigma
-    
-    ## Setting up the function that we will ultimately solve to get the sample size
-    powerF <- function(n){
-        ## Lambda as a function of x (i.e., the sample size)
-        lambda <- function(x){
-            as.numeric(x * t(beta) %*% solve(solve(M.Matrix) %*% Sigma.Matrix %*% t(solve(M.Matrix))) %*% beta)
-        }
-        
-        righthand.side <- pf(q=qf(p=(1-gamma), df1=p, df2=n-q-p), df1=p, df2=n-q-p, ncp=lambda(n))
-        lefthand.side = b
-        return(righthand.side - lefthand.side)
-    }
-    
-    sample_size <- uniroot(powerF, lower=p+q+1, upper=1000000)$root
+                                        b = 1 - power,
+                                        exact = FALSE)
     return(sample_size)
 }
+
 
 # Calculate sample size with given sample size: wrapper function
 calculatePowerBinaryMRT_wrapper <- function(p10, pT0, p11, pT1,
@@ -126,7 +94,7 @@ calculatePowerBinaryMRT_wrapper <- function(p10, pT0, p11, pT1,
         f_t <- cbind(1, 1:total_T)
     }
     
-    power <- MRT_bin_power.F(avail_pattern = avail_pattern,
+    power <- calculate_mrt_bin_power_f(avail_pattern = avail_pattern,
                              f_t = f_t,
                              g_t = g_t,
                              beta = alpha_beta_ate$beta,
@@ -137,72 +105,6 @@ calculatePowerBinaryMRT_wrapper <- function(p10, pT0, p11, pT1,
     return(power)
 }
 
-# Calculate the power with given sample size
-MRT_bin_power.F <- function(avail_pattern,  ## E[I_t]  # TQ: will assume this is vector of length T
-                            f_t,             ## f(t)    # TQ: will assume this f_t object is matrix of dimension T * p
-                            g_t,             ## g(t)    # TQ: will assume this g_t object is matrix of dimension T * q
-                            beta,            ## beta_0
-                            alpha,           ## alpha_0
-                            p_t,             ## p_t (randomization probability)
-                            gamma,          ## gamma (type I error),
-                            sample_size)     ## sample size
-{
-    
-    p <- length(beta)
-    q <- length(alpha)
-    
-    M_and_Sigma <- computeMSigma(avail_pattern, f_t, g_t, beta, alpha, p_t)
-    M.Matrix <- M_and_Sigma$M
-    Sigma.Matrix <- M_and_Sigma$Sigma
-    
-    ## Setting up the function that we will ultimately solve to get the sample size
-    n <- sample_size
-    lambda <- as.numeric(n * t(beta) %*% solve(solve(M.Matrix) %*% Sigma.Matrix %*% t(solve(M.Matrix))) %*% beta)
-    b <- pf(q=qf(p=(1-gamma), df1=p, df2=n-q-p), df1=p, df2=n-q-p, ncp=lambda)
-    return(1 - b)
-}
-
-
-computeMSigma <- function(avail_pattern,  ## E[I_t]  # TQ: will assume this is vector of length T
-                          f_t,             ## f(t)    # TQ: will assume this f_t object is matrix of dimension T * p
-                          g_t,             ## g(t)    # TQ: will assume this g_t object is matrix of dimension T * q
-                          beta,            ## beta_0
-                          alpha,           ## alpha_0
-                          p_t              ## p_t
-) {
-    p <- length(beta)
-    q <- length(alpha)
-    
-    ## The M and Sigma matrices (needed to compute lambda)
-    M.Matrix <- matrix(data=0, nrow=length(beta), ncol=length(beta))
-    Sigma.Matrix <- matrix(data=0, nrow=length(beta), ncol=length(beta))
-    for (i in 1:length(p_t)){## For each decision point (T = total # of decision points is taken as the length of p_t, for now)
-        
-        # breaking down steps to identify bug and improve robustness of code
-        this_f_t <- as.numeric(f_t[i, ])
-        this_g_t <- as.numeric(g_t[i, ])
-        
-        stopifnot(length(this_f_t) == p)
-        stopifnot(length(this_g_t) == q)
-        
-        this_f_t_times_beta <- sum(this_f_t * beta) # both are vectors, so use this way to calculate inner product
-        this_g_t_times_alpha <- sum(this_g_t * alpha)
-        
-        this_f_t_f_t <- outer(this_f_t, this_f_t) # this is f_t %*% f_t^T
-        
-        ThisM <- as.numeric(avail_pattern[i] * (exp(p_t[i] * this_f_t_times_beta)) * exp(this_g_t_times_alpha) * (1-p_t[i]) * p_t[i]) * this_f_t_f_t
-        stopifnot("matrix" %in% class(ThisM) & all(dim(ThisM) == c(p, p))) # added a check of dimension
-        
-        M.Matrix <- M.Matrix + ThisM  ## A running sum so that we end up with each entry being the sum of that entry across all time points
-        
-        ThisSigma <- as.numeric(avail_pattern[i] * (exp(2 * p_t[i] * this_f_t_times_beta)) * exp(this_g_t_times_alpha) * (1-p_t[i]) * p_t[i] * ((1-p_t[i]) * exp(-1 * this_f_t_times_beta) + p_t[i] - exp(this_g_t_times_alpha))) * this_f_t_f_t
-        Sigma.Matrix <- Sigma.Matrix + ThisSigma
-        stopifnot("matrix" %in% class(ThisSigma) & all(dim(ThisSigma) == c(p, p)))
-    }
-    
-    return(list(M = M.Matrix, Sigma = Sigma.Matrix))
-    
-}
 
 ## For computing the generative model from user inputs
 compute_alpha_beta_from_prob <- function(p10, pT0, p11, pT1, total_T,
